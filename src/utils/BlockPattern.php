1653f6ddb5141807848273e60ad27310d0c507af
pixelwhiz <daffa01futn@gmail.com>
2024-10-12 15:11:04 +0700

    initial commit

--- src/utils/BlockPattern.php
+++ src/utils/BlockPattern.php
@@ -11,41 +11,40 @@ use pocketmine\world\World;
 class BlockPattern {
 
     public static function clearPattern(World $world, Position $pos) : void {
-        $blocksPos = [
-            ## GOLD ##
-            $world->getBlock($pos->subtract(1, 2, 0)->add(0, 0, 1))->getPosition(),
-            $world->getBlock($pos->subtract(0, 2, 1)->add(0, 0, 0))->getPosition(),
-            $world->getBlock($pos->subtract(1, 2, 1)->add(0, 0, 0))->getPosition(),
+        
+        $blocks = [
+            $world->getBlock($pos->subtract(1, 2, 0)->add(0, 0, 1)),
+            $world->getBlock($pos->subtract(0, 2, 1)->add(0, 0, 0)),
+            $world->getBlock($pos->subtract(1, 2, 1)->add(0, 0, 0)),
 
-            $world->getBlock($pos->subtract(1, 2, 0)->add(0, 0, 0))->getPosition(),
-            $world->getBlock($pos->subtract(0, 2, 0)->add(0, 0, 0))->getPosition(),
-            $world->getBlock($pos->subtract(0, 2, 0)->add(0, 0, 1))->getPosition(),
+            $world->getBlock($pos->subtract(1, 2, 0)->add(0, 0, 0)),
+            $world->getBlock($pos->subtract(0, 2, 0)->add(0, 0, 0)),
+            $world->getBlock($pos->subtract(0, 2, 0)->add(0, 0, 1)),
 
-            $world->getBlock($pos->subtract(0, 2, 0)->add(1, 0, 1))->getPosition(),
-            $world->getBlock($pos->subtract(0, 2, 0)->add(1, 0, 0))->getPosition(),
-            $world->getBlock($pos->subtract(0, 2, 1)->add(1, 0, 0))->getPosition(),
+            $world->getBlock($pos->subtract(0, 2, 0)->add(1, 0, 1)),
+            $world->getBlock($pos->subtract(0, 2, 0)->add(1, 0, 0)),
+            $world->getBlock($pos->subtract(0, 2, 1)->add(1, 0, 0)),
             ## GOLD ##
 
             ## REDSTONE ##
-            $world->getBlock($pos->subtract(1, 1, 0)->add(0, 0, 1))->getPosition(),
-            $world->getBlock($pos->subtract(0, 1, 1)->add(0, 0, 0))->getPosition(),
-            $world->getBlock($pos->subtract(1, 1, 1)->add(0, 0, 0))->getPosition(),
+            $world->getBlock($pos->subtract(1, 1, 0)->add(0, 0, 1)),
+            $world->getBlock($pos->subtract(0, 1, 1)->add(0, 0, 0)),
+            $world->getBlock($pos->subtract(1, 1, 1)->add(0, 0, 0)),
 
-            $world->getBlock($pos->subtract(1, 1, 0)->add(0, 0, 0))->getPosition(),
-            $world->getBlock($pos->subtract(0, 1, 0)->add(0, 0, 0))->getPosition(),
-            $world->getBlock($pos->subtract(0, 1, 0)->add(0, 0, 1))->getPosition(),
+            $world->getBlock($pos->subtract(1, 1, 0)->add(0, 0, 0)),
+            $world->getBlock($pos->subtract(0, 1, 0)->add(0, 0, 0)),
+            $world->getBlock($pos->subtract(0, 1, 0)->add(0, 0, 1)),
 
-            $world->getBlock($pos->subtract(0, 1, 0)->add(1, 0, 1))->getPosition(),
-            $world->getBlock($pos->subtract(0, 1, 0)->add(1, 0, 0))->getPosition(),
-            $world->getBlock($pos->subtract(0, 1, 1)->add(1, 0, 0))->getPosition(),
-            ## REDSTONE ##
-
-            $world->getBlock($pos->subtract(0, 1, 0)->add(0, 0, 0))->getPosition(),
-            $world->getBlock($pos->subtract(0, 0, 0)->add(0, 0, 0))->getPosition(),
+            $world->getBlock($pos->subtract(0, 1, 0)->add(1, 0, 1)),
+            $world->getBlock($pos->subtract(0, 1, 0)->add(1, 0, 0)),
+            $world->getBlock($pos->subtract(0, 1, 1)->add(1, 0, 0)),
         ];
 
-        foreach ($blocksPos as $blockPos) {
-            $world->setBlock($blockPos, VanillaBlocks::AIR());
+        foreach ($blocks as $block) {
+            if ($block instanceof Block) {
+                $world->addParticle($block->getPosition(), new BlockBreakParticle($block), $world->getPlayers());
+                $world->setBlock($block->getPosition(), VanillaBlocks::AIR());
+            }
         }
     }
 
